57dad2e717309b9f3fd6b36be1c32cd162770904
antonioreyes22 <antonio.delosreyesperez@alum.uca.es>
2026-04-23 19:05:12 +0200

    CRUD V0.1

--- Frontend/src/app/components/crud/crud.ts
+++ Frontend/src/app/components/crud/crud.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-crud',
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './crud.html',
   styleUrl: './crud.css',
 })
