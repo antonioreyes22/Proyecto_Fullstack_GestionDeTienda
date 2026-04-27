1ddec1ed1e4280140da11f07015c1fdf13502838
antonioreyes22 <antonio.delosreyesperez@alum.uca.es>
2026-04-27 20:51:01 +0200

    Galeria de imagenes en Inicio

--- Frontend/src/app/components/inicio/inicio.ts
+++ Frontend/src/app/components/inicio/inicio.ts
@@ -1,11 +1,63 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
 export class Inicio {
 
+  listaMuestra: string[] = [];
+  centralIndex: number = 0;
+  
+  constructor()
+  {
+    this.listaMuestra = 
+    [
+      'img/camiseta1.png',
+      'img/camiseta2.png',
+      'img/camiseta3.png',
+    ]
+  }
+
+  next()
+  {
+    if((this.centralIndex+1) >= this.listaMuestra.length)
+      {
+        this.centralIndex = 0;
+      }
+      else this.centralIndex++; 
+  }
+
+  before(){
+    if((this.centralIndex-1) < 0)
+      {
+        this.centralIndex = this.listaMuestra.length-1;
+      }
+      else this.centralIndex--; 
+  }
+
+  get centro(){
+    return this.listaMuestra[this.centralIndex];
+  }
+
+  get izquierda()
+  {
+    if((this.centralIndex - 1) < 0)
+      {
+        return this.listaMuestra[this.listaMuestra.length-1];
+      }
+      else return this.listaMuestra[this.centralIndex - 1];
+  }
+  
+  get derecha()
+  {
+    if((this.centralIndex + 1) >= this.listaMuestra.length)
+      {
+        return this.listaMuestra[0];
+      }
+      else return this.listaMuestra[this.centralIndex + 1];
+  }
 }
